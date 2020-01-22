ee1cf5bd969d690e09118ebc4e6b56471bb57eef
Osman Armando Jimenez Cortes <33326926+OsmanJimenez@users.noreply.github.com>
2020-01-22 09:29:08 -0500

    My Account Login Update

--- app/components/Account/LoginForm.js
+++ app/components/Account/LoginForm.js
@@ -2,25 +2,38 @@ import React, { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { Input, Icon, Button } from 'react-native-elements';
 import { validateEmail } from '../../utils/Validation';
+import { withNavigation } from 'react-navigation';
+import * as firebase from 'firebase'
+import Loading from '../Loading';
 
 
-
-export default function LoginForm(props){
-    const { toastRef }= props;
+function LoginForm(props){
+    const { toastRef, navigation }= props;
     const [hidePassword, setHidePassword] = useState(true);
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [isVisibleLoading, setIsVisibleLoading] = useState(false);
 
-    const login = () => {
+    const login = async () => {
+        setIsVisibleLoading(true);
         if(!email || !password ){
             toastRef.current.show('Todos los campos son obligatorios');
         } else {
             if(!validateEmail(email)){
                 toastRef.current.show('El correo no es correcto');
             }else{
-                console.log('Login Correcto...');
+                await firebase
+                    .auth()
+                    .signInWithEmailAndPassword(email, password)
+                    .then(() => {
+                        navigation.navigate('Account');
+                    })
+                    .catch(() => {
+                        toastRef.current.show('Email o contraseña incorrecta')
+                    })
                 }
             }
+        setIsVisibleLoading(false);
     };
 
     return(
@@ -58,10 +71,13 @@ export default function LoginForm(props){
                 containerStyle={styles.btnContainerLogin}
                 buttonStyle={styles.btnLogin}
                 onPress={login}
-            />            
+            />    
+            <Loading isVisible={isVisibleLoading} text='Iniciando sesión' />        
         </View>
     )
 }
+
+export default withNavigation(LoginForm);
 
 const styles = StyleSheet.create({
     formContainer:{
